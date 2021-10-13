9c7375f0c3f85c3287d966a2aec6943f3759f7cd
Elton Villa <euvilla@miners.utep.edu>
2021-10-12 20:21:51 -0600

    Not using custom tokenizer due to segmentation fault error.

--- riscv.c
+++ riscv.c
@@ -93,6 +93,16 @@ void write_read_demo(){
 	printf("Read address %lu (0x%lX): %lu (0x%lX)\n", address, address, read, read); // %lu -> format as an long-unsigned
 }
 
+void print_regs(){
+	int col_size = 10;
+	for(int i = 0; i < 8; i++){
+		printf("X%02i:%.*lld", i, col_size, (long long int) reg[i]);
+		printf(" X%02i:%.*lld", i+8, col_size, (long long int) reg[i+8]);
+		printf(" X%02i:%.*lld", i+16, col_size, (long long int) reg[i+16]);
+		printf(" X%02i:%.*lld\n", i+24, col_size, (long long int) reg[i+24]);
+	}
+}
+
 /**
  * Your code goes in the main
  *
@@ -101,12 +111,12 @@ int main(){
 	// Do not write any code between init_regs
 	init_regs(); // DO NOT REMOVE THIS LINE
 
-	//print_regs();
+	print_regs();
 
 	// Below is a sample program to a write-read. Overwrite this with your own code.
 	write_read_demo();
 
-	printf("RV32 Interpreter.\nType RV32 instructions. Use upper-case letters and space as a delimeter.\nEnter 'EOF' character to end program.\n");
+	printf(" RV32 Interpreter.\nType RV32 instructions. Use upper-case letters and space as a delimeter.\nEnter 'EOF' character to end program.\n");
 
 	char* instruction = malloc(1000 * sizeof(char));
 	bool is_null = false;
@@ -114,19 +124,18 @@ int main(){
 	//fgets() returns null if EOF is reached.
 	is_null = fgets(instruction, 1000, stdin) == NULL;
 	while(!is_null){
+		interpret(instruction);
+		//printf("\nTokenizing by space:\n%s\n", tokenize(instruction, ' '));
+		//printf("Tokenize time\n");
+		//printf("\nTokenizing by letter a:\n%s\n", tokenize(instruction, 'a'));
 
-	printf("Your instruction is: %s\n", instruction);
-	interpret(instruction);
-	printf("\nTokenizing by space:\n%s\n", tokenize(instruction, ' '));
-	printf("\nTokenizing by letter a:\n%s\n", tokenize(instruction, 'a'));
-	/**
-	printf("\n");
-	print_regs();
-	printf("\n");
-	**/
-	is_null = fgets(instruction, 1000, stdin) == NULL;
+		printf("\n");
+		print_regs();
+		printf("\n");
+
+		is_null = fgets(instruction, 1000, stdin) == NULL;
 	}
 
-	printf("Graceful exit.\n");
+	printf("Good bye!\n");
 	return 0;
 }
